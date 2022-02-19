80edff2bb15c53a3dd0b4e7f361d41871156bf8a
Nicholas Schense <nschense@gmail.com>
2022-02-18 21:18:57 -0600

    Added chassis PIDSparkMaxes and PID constants

--- src/main/java/frc/robot/subsystems/Chassis.java
+++ src/main/java/frc/robot/subsystems/Chassis.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import frc.robot.util.PIDSparkMotor;
 import static frc.robot.Constants.*;
 
 public class Chassis extends SubsystemBase {
@@ -11,31 +12,37 @@ public class Chassis extends SubsystemBase {
     private CANSparkMax frontRightMotor;
     private CANSparkMax rearLeftMotor;
     private CANSparkMax rearRightMotor;
+    private PIDSparkMotor leftPIDSparkMotor;
+    private PIDSparkMotor rightPIDSparkMotor;
 
     public Chassis() {
         frontLeftMotor = new CANSparkMax(CHASSIS_FRONT_LEFT_MOTOR_CAN_ID, MotorType.kBrushless);
-
         frontLeftMotor.restoreFactoryDefaults();
         frontLeftMotor.setInverted(false);
         frontLeftMotor.setIdleMode(IdleMode.kCoast);
 
-        frontRightMotor = new CANSparkMax(CHASSIS_FRONT_RIGHT_MOTOR_CAN_ID, MotorType.kBrushless);
+        leftPIDSparkMotor = new PIDSparkMotor(frontLeftMotor, CHASSIS_DRIVE_P, CHASSIS_DRIVE_I, CHASSIS_DRIVE_D);
 
+        frontRightMotor = new CANSparkMax(CHASSIS_FRONT_RIGHT_MOTOR_CAN_ID, MotorType.kBrushless);
         frontRightMotor.restoreFactoryDefaults();
         frontRightMotor.setInverted(false);
         frontRightMotor.setIdleMode(IdleMode.kCoast);
 
-        rearLeftMotor = new CANSparkMax(CHASSIS_REAR_LEFT_MOTOR_CAN_ID, MotorType.kBrushless);
+        rightPIDSparkMotor = new PIDSparkMotor(frontRightMotor, CHASSIS_DRIVE_P, CHASSIS_DRIVE_I, CHASSIS_DRIVE_D);
 
+        rearLeftMotor = new CANSparkMax(CHASSIS_REAR_LEFT_MOTOR_CAN_ID, MotorType.kBrushless);
         rearLeftMotor.restoreFactoryDefaults();
         rearLeftMotor.setInverted(false);
         rearLeftMotor.setIdleMode(IdleMode.kCoast);
 
-        rearRightMotor = new CANSparkMax(CHASSIS_REAR_RIGHT_MOTOR_CAN_ID, MotorType.kBrushless);
+        rearLeftMotor.follow(frontLeftMotor);
 
+        rearRightMotor = new CANSparkMax(CHASSIS_REAR_RIGHT_MOTOR_CAN_ID, MotorType.kBrushless);
         rearRightMotor.restoreFactoryDefaults();
         rearRightMotor.setInverted(false);
         rearRightMotor.setIdleMode(IdleMode.kCoast);
+
+        rearRightMotor.follow(frontRightMotor);
     }
 
     @Override
