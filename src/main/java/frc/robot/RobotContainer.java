34bfa3fb6162e1e52e2ba4c4a7d740bd462cea24
Keith <KeithRieck@gmail.com>
2022-02-19 09:56:11 -0600

    Added compressor

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,39 +5,54 @@ import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.XboxController;
 
+import static frc.robot.Constants.*;
 
 /**
- * This class is where the bulk of the robot should be declared.  Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
- * (including subsystems, commands, and button mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very little robot logic
+ * should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including subsystems, commands, and button mappings)
+ * should be declared here.
  */
 public class RobotContainer {
 
   private static RobotContainer m_robotContainer = new RobotContainer();
 
-// The robot's subsystems
-    public final Shooter m_shooter = new Shooter();
-    public final Acquisition m_acquisition = new Acquisition();
-    public final Climber m_climber = new Climber();
-    public final Chassis m_chassis = new Chassis();
+  // The robot's subsystems
+  public final Shooter m_shooter = new Shooter();
+  public final Acquisition m_acquisition = new Acquisition();
+  public final Climber m_climber = new Climber();
+  public final Chassis m_chassis = new Chassis();
 
-// Joysticks
-private final XboxController xController2 = new XboxController(1);
-private final XboxController xController1 = new XboxController(0);
+  // Joysticks
+  private final XboxController xController2 = new XboxController(1);
+  private final XboxController xController1 = new XboxController(0);
 
-  
+  // Pneumatics
+  private Compressor compressor;
+
   // A chooser for autonomous commands
   SendableChooser<Command> m_chooser = new SendableChooser<>();
 
   /**
-  * The container for the robot.  Contains subsystems, OI devices, and commands.
-  */
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   private RobotContainer() {
-    // Smartdashboard Subsystems
 
+    // Pneumatics
+    if (PRACTICE_ROBOT) {
+      compressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
+    } else {
+      compressor = new Compressor(0, PneumaticsModuleType.REVPH);
+    }
+    compressor.enableDigital();
+
+    // Smartdashboard Subsystems
 
     // SmartDashboard Buttons
     SmartDashboard.putData("Autonomous Command", new AutonomousCommand());
@@ -51,7 +66,6 @@ private final XboxController xController1 = new XboxController(0);
 
     m_chooser.setDefaultOption("Autonomous Command", new AutonomousCommand());
 
-
     SmartDashboard.putData("Auto Mode", m_chooser);
   }
 
@@ -60,26 +74,25 @@ private final XboxController xController1 = new XboxController(0);
   }
 
   /**
-   * Use this method to define your button->command mappings.  Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by instantiating a {@link GenericHID} or one of its subclasses
+   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and
+   * then passing it to a
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-  
-// Create some buttons
+
+    // Create some buttons
   }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
-  */
+   */
   public Command getAutonomousCommand() {
     // The selected command will be run in autonomous
     return m_chooser.getSelected();
   }
-  
 
 }
-
