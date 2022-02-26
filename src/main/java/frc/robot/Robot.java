c5b4e024e4715b1567dcc60398381b1c22cba21b
Theo-X <albanot14@gmail.com>
2022-02-25 21:22:30 -0600

    Update Robot.java
    Added Basic Lights Interface

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,11 +6,27 @@ import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.I2C.Port;
 
 public class Robot extends TimedRobot {
 
     private Command m_autonomousCommand;
     private RobotContainer m_robotContainer;
+
+    public static int I2C_ADDRESS = 0x41;
+    public static final int MAX_ANIMATIONS = 3;
+    public static final int MAX_STRIPS = 4;
+
+    private byte[] currentAnimation = new byte[MAX_STRIPS];
+    private byte[] nextAnimation = new byte[MAX_STRIPS];
+    private int myStrip = 0;
+    private int myAnim = 0;
+    private byte[] dataOut = new byte[1];
+
+    private XboxController xbox = null;
+    private I2C i2c = null;
 
     @Override
     public void robotInit() {
@@ -19,6 +35,55 @@ public class Robot extends TimedRobot {
                 "/u/config.properties");
         m_robotContainer = RobotContainer.getInstance();
         HAL.report(tResourceType.kResourceType_Framework, tInstances.kFramework_RobotBuilder);
+        xbox = new XboxController(0);
+        i2c = new I2C(Port.kMXP, I2C_ADDRESS);
+        resetAnimations();
+    }
+
+    public void teleopPeriodic() {
+        if (xbox.getLeftBumperPressed()) {
+          myAnim = (myAnim + 1) % MAX_ANIMATIONS;
+          setAnimation(myStrip, myAnim);
+        } else if (xbox.getRightBumperPressed()) {
+          resetAnimations();
+        } else if (xbox.getYButtonPressed()) {
+          myStrip = 0;
+        } else if (xbox.getBButtonPressed()) {
+          myStrip = 1;
+        } else if (xbox.getAButtonPressed()) {
+          myStrip = 2;
+        } else if (xbox.getXButtonPressed()) {
+          myStrip = 3;
+        }
+        sendAllAnimations();
+    }
+
+    private void resetAnimations() {
+        for (int s = 0; s < MAX_STRIPS; s++) {
+          nextAnimation[s] = (byte)0x0f;
+          currentAnimation[s] = 0;
+        }
+        myStrip = 0;
+        myAnim = 0;
+    }
+    
+    private void setAnimation(int stripNumber, int animNumber) {
+        Integer b = Integer.valueOf(((stripNumber << 4) & 0xF0) | (animNumber & 0x0F));
+        nextAnimation[stripNumber] = b.byteValue();
+    }
+
+    private void sendAllAnimations() {
+        for (int s = 0; s < MAX_STRIPS; s++) {
+          if (nextAnimation[s] != currentAnimation[s]) {
+            sendOneAnimation(s);
+            currentAnimation[s] = nextAnimation[s];
+          }
+        }
+    }
+
+    private void sendOneAnimation(int stripNumber) {
+        dataOut[0] = nextAnimation[stripNumber];
+        i2c.writeBulk(dataOut, dataOut.length);
     }
 
     @Override
@@ -51,10 +116,6 @@ public class Robot extends TimedRobot {
         if (m_autonomousCommand != null) {
             m_autonomousCommand.cancel();
         }
-    }
-
-    @Override
-    public void teleopPeriodic() {
     }
 
     @Override
