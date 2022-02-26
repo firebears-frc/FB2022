fe456092e3a9527a8c7dd5d8d781d7ebe548f1a1
Functian <ianhauer@icloud.com>
2022-02-26 16:15:37 -0600

    made a auto drive command using ultrasonic

---
+++ src/main/java/frc/robot/commands/DriveToWallCommand.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import org.ejml.interfaces.decomposition.DecompositionSparseInterface;
+
+import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.simulation.UltrasonicSim;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Chassis;
+
+public class DriveToWallCommand extends CommandBase {
+  private Chassis m_chassis;
+  private double distance;
+
+  /** Creates a new DriveToWallCommand. */
+  public DriveToWallCommand(Chassis c, double d) {
+    m_chassis = c;
+    distance = d;
+
+    addRequirements(m_chassis);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_chassis.arcadeDrive(0.7, 0);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_chassis.arcadeDrive(0, 0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return m_chassis.getUltrasonicDistanceInches() < distance;
+  }
+}
