b782cfffde624aa32409f3db084a0a68a2d4f8fd
Functian <ianhauer@icloud.com>
2022-02-27 15:29:44 -0600

    made it move backwards during autonomous

--- src/main/java/frc/robot/commands/AutonomousCommand.java
+++ src/main/java/frc/robot/commands/AutonomousCommand.java
@@ -1,35 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotContainer;
 
-public class AutonomousCommand extends CommandBase {
+public class AutonomousCommand extends SequentialCommandGroup {
 
-    public AutonomousCommand() {
-    }
-
-    // Called when the command is initially scheduled.
-    @Override
-    public void initialize() {
-    }
-
-    // Called every time the scheduler runs while the command is scheduled.
-    @Override
-    public void execute() {
-    }
-
-    // Called once the command ends or is interrupted.
-    @Override
-    public void end(boolean interrupted) {
-    }
-
-    // Returns true when the command should end.
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
-
-    @Override
-    public boolean runsWhenDisabled() {
-        return false;
-    }
+  public AutonomousCommand() {
+    addCommands(
+      new DriveToDistancePIDCommand(-60, RobotContainer.getInstance().m_chassis)
+    );
+  }
 }
