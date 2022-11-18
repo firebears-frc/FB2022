c877de3882c94247fe8fbb026710618781629eba
Sadiq Ahmed <ahmedsadiq@apps.isd623.org>
2022-11-17 20:24:23 -0600

    Added new Vision command & removed shootvisioncommand

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -53,8 +53,8 @@ public class RobotContainer {
   public final Climber m_climber = new Climber();
   public final Chassis m_chassis = new Chassis(powerDistribution);
   public final Lights m_lights = new Lights();
-  public final AHRS m_navx = new AHRS(SerialPort.Port.kUSB);
-  public final Vision m_vision = new Vision("CameraName", 0, Units.metersToFeet(8), 0, m_navx);
+  //public final AHRS m_navx = new AHRS(SerialPort.Port.kUSB);
+  public final Vision m_vision = new Vision("mmal_service_16.1");
 
   // Joysticks
   private final Joystick joystick = new Joystick(0);
@@ -231,6 +231,11 @@ public class RobotContainer {
 
     //final POVButton povButtonRight = new POVButton(xController, 90);
     //povButtonRight.whenPressed(new ClimberReachOutCommand(m_climber), true);
+
+    /*
+    final JoystickButton VButton = new JoystickButton(joystick,7);
+    VButton.whileHeld(new DriveTo(m_vision, m_chassis));
+    */
   }
 
   /**
--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -21,8 +21,6 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTablesJNI;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import com.kauailabs.navx.frc.*;
-
 import static frc.robot.Constants.*;
 
 import java.util.List;
@@ -32,17 +30,17 @@ public class Vision extends SubsystemBase {
 
   PhotonCamera photonCam;
   PhotonTrackedTarget photonTarget;
-  NetworkTablesJNI nt;
+  //NetworkTablesJNI nt;
 
   // gyro
-  AHRS navX;
-
+  /*
   double robotYaw;
   double visionYaw;
 
   private double camHeight;
   private double targetHeight;
   private double camPitch;
+  */
 
   public double TargetDist = -1;
   public double TargetX = 0;
@@ -55,21 +53,11 @@ public class Vision extends SubsystemBase {
   /**
    *
    * @param CameraName - The Name Of The Camera
-   * @param cH         - Camera Height : How High The Camera Is Off The Ground
-   * @param tH         - Target Height : How Tall The Target Is , (8ft this year)
-   * @param cP         - Camera Pitch : The Angle Of The Camera,
-   * @param gyro       - Gyro NavX System Subsystem - Used To Detect The Rotation
-   *                   Of The Robot
    */
-  public Vision(String CameraName, double cH, double tH, double cP, AHRS gyro) {
+  public Vision(String CameraName) {
     if (VISION_ENABLED) {
       photonCam = new PhotonCamera(CameraName);
-      nt = new NetworkTablesJNI();
     }
-    camHeight = cH;
-    targetHeight = tH;
-    camPitch = cP;
-    navX = gyro;
 
     SmartDashboard.putString("Pos", "init");
     SmartDashboard.putString("Rot", "init");
@@ -84,10 +72,6 @@ public class Vision extends SubsystemBase {
     if (VISION_ENABLED) {
       // This method will be called once per scheduler run
       var result = photonCam.getLatestResult();
-
-      robotYaw = (navX.getAngle()) %  360;
-
-      SmartDashboard.putNumber("Robot Yaw", robotYaw % 360);
 
       if (result.hasTargets()) {
         photonTarget = photonCam.getLatestResult().getBestTarget();
@@ -124,93 +108,5 @@ public class Vision extends SubsystemBase {
         TargetRot = 0;
       }
     }
-  }
-
-  /* Functions That Return Values To Main.Java */
-
-  /**
-   *
-   * @return Returns The Best Target From PhotonVision IE: Largest, Center-Most
-   *         Target
-   */
-  public PhotonTrackedTarget getBestTarget() {
-    return photonTarget;
-  }
-
-  /**
-   *
-   * @return Returns The Target Yaw From -1 To 1, Based On CameraFOV
-   */
-  public double getAngle() {
-    if (photonTarget != null) {
-      return photonTarget.getYaw()/CameraFOV;
-    } else {
-      return 0;
-    }
-  }
-
-  /**
-   *
-   * @return Returns The Target Yaw (RAW), From CameraFOV *-30 To *30
-   */
-  public double getAngleRaw() {
-    if (photonTarget != null) {
-      return photonTarget.getYaw()/CameraFOV;
-    }
-    else {
-      return 0;
-    }
-  }
-
-  /**
-   *
-   * @return Returns The Distence, Based Off Robot's Camera and Target Height, Camera and Target Pitch, Returns In Inches *In.
-   */
-  public double getDistence() {
-    if (photonTarget == null) return 0;
-    return Units.metersToInches(PhotonUtils.calculateDistanceToTargetMeters(
-            camHeight,
-            targetHeight,
-            camPitch,
-            Units.degreesToRadians(photonTarget.getPitch())));
-  }
-
-  void updateVisionYaw() {
-    if (photonTarget == null)
-      return;
-    visionYaw = getAngleRaw();
-  }
-
-  /**
-   *
-   * @return Returns The Last 'Known' Angle Of The Robot, Returns The Angle Based
-   *         Off Of The Robots Yaw, Robot Is North And Target Will Be Position
-   *         Likewise **Untested
-   */
-  public double getLastAngleRaw() {
-    System.out.println(visionYaw + robotYaw);
-    return visionYaw + robotYaw;
-  }
-
-  /**
-   *
-   * @return Returns The Targets Yaw Based On The Robots Gyro's Angle And The Last 'Known' Angle of The Vision Target
-   */
-  public double getLastAngle() {
-    System.out.println(visionYaw + robotYaw);
-    return -((visionYaw + robotYaw) % 360);
-  }
-
-  /**
-   *
-   * @return Returns True Or False Based On The Vision Targets Distence From The Robot, From x-y Amount Of Inches Being True, Else Being False
-   */
-  public boolean inRange(){
-    if(photonTarget == null){
-      return false;
-    }
-    double distance = getDistence();
-    //change the range, currently 60in to 160in
-    return (60 <= distance && distance <= 160);
   }
 }
